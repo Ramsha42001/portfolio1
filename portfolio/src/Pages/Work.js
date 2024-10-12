eb60d990cd3c8dfa1e53e8a3516a4673f8d51ca2
Ramsha42001 <90575580+Ramsha42001@users.noreply.github.com>
2024-10-12 20:50:53 +0530

    changed modal

--- portfolio/src/Pages/Work.js
+++ portfolio/src/Pages/Work.js
@@ -16,15 +16,16 @@ import {
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import course from '../images/course.png'
-import gdsc from '../images/gdsc.png'
-import ieee from '../images/ieee.png'
-import lnm from '../images/lnmHacks.png'
-import phoenix from '../images/phoenix.png'
-import plinth from '../images/plinth.png'
-import car from '../images/car.png'
-import ats1 from '../images/ats1.png'
-import hr from '../images/hr.png'
+import course from '../images/course.png';
+import gdsc from '../images/gdsc.png';
+import ieee from '../images/ieee.png';
+import lnm from '../images/lnmHacks.png';
+import phoenix from '../images/phoenix.png';
+import plinth from '../images/plinth.png';
+import car from '../images/car.png';
+import ats1 from '../images/ats1.png';
+import hr from '../images/hr.png';
+
 const MotionBox = motion(Box);
 const MotionText = motion(Text);
 const MotionHeading = motion(Heading);
@@ -33,25 +34,25 @@ const categories = [
   {
     title: "Development",
     projects: [
-      { name: "Faculty course Website", src: course, github:'https://github.com/Ramsha42001/course_website'},
-      { name: "GDSC Website", src: gdsc ,github:'https://github.com/Ramsha42001/gdsc-website-1'},
-      { name: "IEEE LNMIIT Official website", src:ieee ,github:''},
-      { name: "LNM Hacks Website", src: lnm ,github:'https://github.com/Ramsha42001/lnm-hacks'},
-      { name: "Phoenix Club Website", src: phoenix,github:'https://phoenix.lnmiit.ac.in/' },
-      { name: "Plinth Tech Fest LNMIIT", src: plinth ,github:''},
+      { name: "Faculty course Website", src: course, github: 'https://github.com/Ramsha42001/course_website' },
+      { name: "GDSC Website", src: gdsc, github: 'https://github.com/Ramsha42001/gdsc-website-1' },
+      { name: "IEEE LNMIIT Official website", src: ieee, github: '' },
+      { name: "LNM Hacks Website", src: lnm, github: 'https://github.com/Ramsha42001/lnm-hacks' },
+      { name: "Phoenix Club Website", src: phoenix, github: 'https://phoenix.lnmiit.ac.in/' },
+      { name: "Plinth Tech Fest LNMIIT", src: plinth, github: '' },
     ],
   },
   {
     title: "Data Science",
     projects: [
-      { name: "Used Car Price Prediction", src: car,github:'https://github.com/Ramsha42001/Data-Science' },
-      { name: "ATS Resume Scorer Web Application", src: ats1 ,github:''},
+      { name: "Used Car Price Prediction", src: car, github: 'https://github.com/Ramsha42001/Data-Science' },
+      { name: "ATS Resume Scorer Web Application", src: ats1, github: '' },
     ],
   },
   {
     title: "Data Analysis",
     projects: [
-      { name: "HR Analytics Dashboard Power BI", src: hr,github:'https://drive.google.com/file/d/1vT-p2wlds9F43AxXaGaxWagVxTFaXi68/view?usp=drive_link' },
+      { name: "HR Analytics Dashboard Power BI", src: hr, github: 'https://drive.google.com/file/d/1vT-p2wlds9F43AxXaGaxWagVxTFaXi68/view?usp=drive_link' },
     ],
   },
 ];
@@ -119,8 +120,19 @@ function Work() {
       >
         {"<"}h2{"/>"}
       </MotionText>
+
+      {/* Box containing the project cards */}
       <Box w="100%" minH="70vh" h="auto" py={5}>
-        <Box w="100%" minH="50vh" h='auto' display="flex" flexDirection="row" justifyContent={{base:'center',md:'center',lg:'space-between'}} flexWrap='wrap' alignItems='flex-start'>
+        <Box
+          w="100%"
+          minH="50vh"
+          h="auto"
+          display="flex"
+          flexDirection="row"
+          justifyContent={{ base: "center", md: "center", lg: "space-between" }}
+          flexWrap="wrap"
+          alignItems="center" // Center vertically
+        >
           {categories.map((category, index) => (
             <MotionBox
               key={index}
@@ -129,12 +141,11 @@ function Work() {
               transition={{ duration: 0.5, delay: 1 + index * 0.4 }}
               shadow="custom"
               w={300}
-              h={{base:'200',md:'200',lg:'300'}}
+              h={{ base: '200', md: '200', lg: '300' }}
               border="2px"
               borderColor="white"
               borderRadius="15px"
               m={15}
-              position="relative"
               display="flex"
               flexDirection="column"
               justifyContent="center"
@@ -142,8 +153,12 @@ function Work() {
               onClick={() => handleCardClick(category.projects)}
               cursor="pointer" // Add cursor pointer for better UX
             >
-              <Text fontSize="m" color="heading" fontWeight="bold">{category.title}</Text>
-              <Text fontSize="s" color="heading" fontWeight="normal">(click to see projects)</Text>
+              <Text fontSize="m" color="heading" fontWeight="bold">
+                {category.title}
+              </Text>
+              <Text fontSize="s" color="heading" fontWeight="normal">
+                (click to see projects)
+              </Text>
             </MotionBox>
           ))}
         </Box>
@@ -156,21 +171,21 @@ function Work() {
           <ModalHeader color="white">Projects</ModalHeader> {/* Change header color if needed */}
           <ModalCloseButton color="white" />
           <ModalBody display='flex' flexDirection='row' flexWrap='wrap'>
-  {selectedProjects.map((project, index) => (
-    <Box key={index} display="flex" flexDir='column' alignItems="center" mb={4}>
-      <a href={project.github} target="_blank" rel="noopener noreferrer"> {/* GitHub Link */}
-        <Image
-          border='4px' borderColor='subHeading' shadow="custom"
-          src={project.src} mx={12} my={10} w={{base:250,lg:300}} h={200}
-          alt={project.name} borderRadius="10px"
-          transition="opacity 0.3s ease"
-          _hover={{ opacity: 0.4 }} // Hover effect
-        />
-      </a>
-      <Text ml={4} color="white">{project.name}</Text>
-    </Box>
-  ))}
-</ModalBody>
+            {selectedProjects.map((project, index) => (
+              <Box key={index} display="flex" flexDir='column' alignItems="center" mb={4}>
+                <a href={project.github} target="_blank" rel="noopener noreferrer"> {/* GitHub Link */}
+                  <Image
+                    border='4px' borderColor='subHeading' shadow="custom"
+                    src={project.src} mx={12} my={10} w={{ base: 250, lg: 300 }} h={200}
+                    alt={project.name} borderRadius="10px"
+                    transition="opacity 0.3s ease"
+                    _hover={{ opacity: 0.4 }} // Hover effect
+                  />
+                </a>
+                <Text ml={4} color="white">{project.name}</Text>
+              </Box>
+            ))}
+          </ModalBody>
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
